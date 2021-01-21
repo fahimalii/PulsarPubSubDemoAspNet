9196d9f6823ddbee000386372b454f79d5b38187
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2021-01-21 11:35:13 +0600

    Simple Pub Sub Test with Apache Pulsar in .Net

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using DotPulsar;
+using System;
+using System.Buffers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = PulsarClient.Builder().Build();
+
+            var consumerOptions = new ConsumerOptions("DemoSub", "persistent://public/default/mytopic");
+            var consumer = client.CreateConsumer(consumerOptions);
+
+            await foreach (var message in consumer.Messages())
+            {
+                Console.WriteLine($"Received: {Encoding.UTF8.GetString(message.Data.ToArray())}");
+                //await consumer.Acknowledge(message);
+            }
+
+            //var readerOptions = new ReaderOptions(MessageId.Latest, "persistent://public/default/mytopic");
+            //var reader = client.CreateReader(readerOptions);
+
+            //await foreach (var message2 in reader.Messages())
+            //{
+            //    Console.WriteLine($"Reader Received: {Encoding.UTF8.GetString(message2.Data.ToArray())}");
+            //}
+        }
+    }
+}
---
+++ ProducerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using DotPulsar;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = PulsarClient.Builder().Build();
+
+            var producerOptions = new ProducerOptions("persistent://public/default/mytopic");
+            var producer = client.CreateProducer(producerOptions);
+            while(true)
+            {
+                Console.Write("Please Enter Message: ");
+                var message = Console.ReadLine();
+                var data = Encoding.UTF8.GetBytes(message);
+                var result = await producer.Send(data);
+            }
+        }
+    }
+}
